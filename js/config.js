e108ae431b238840a7bdf467fca0c2c3c77e74ee
Yusuke Hino <hino@partner.sensetime.jp>
2020-08-31 13:38:34 +0900

    change app client

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-west-2_oPt5xAqjP', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4j1q1ut3uih3auo09ogpfc58oh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '1ntapcdktf3ludealamsv625b1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
